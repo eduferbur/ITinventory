96218f6bcd916de49372992671ef480a0c3af814
eduferbur <eduferbur@gmail.com>
2020-11-18 18:31:45 +0100

    Login copiado ok

--- app.py
+++ app.py
@@ -1,20 +1,64 @@
-import sys
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import (Flask,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for
+)
 
-from inventario import users  # inventario es la carpeta, users es el archivo.py
+
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+
+usuario = []
+usuario.append(User(id=1, username='Anthony', password='password'))
+usuario.append(User(id=2, username='Becca', password='secret'))
+usuario.append(User(id=3, username='Carlos', password='somethingsimple'))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/inventory.db'
-db = SQLAlchemy(app)  # Cursor para la base de datos
+app.secret_key = 'somesecretkeythatonlyishouldknow'
 
 
-@app.route('/login')
-def home():
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in usuario if x.id == session['user_id']][0]
+        g.user = user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = [x for x in usuario if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
+
     return render_template('login.html')
+
 
 @app.route('/profile')
 def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+
     return render_template('profile.html')
 
 if __name__ == '__main__':
